3885a081bf4d36461f088112871ee4ce1d4b8232
Christiaan Rakowski <crakowski@e-sites.nl>
2017-05-18 09:43:59 +0200

    File -> New Project(s)

---
+++ src/FeedManager/FeedManager.GrainInterfaces/IGrain1.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Orleans;
+
+namespace FeedManager.GrainInterfaces
+{
+    /// <summary>
+    /// Grain interface IGrain1
+    /// </summary>
+    public interface IGrain1 : IGrainWithGuidKey
+    {
+
+    }
+}
---
+++ src/FeedManager/FeedManager.Grains/Grain1.cs
@@ -0,0 +1,13 @@
+using FeedManager.GrainInterfaces;
+using Orleans;
+
+namespace FeedManager.Grains
+{
+    /// <summary>
+    /// Grain implementation class Grain1.
+    /// </summary>
+    public class Grain1 : Grain, IGrain1
+    {
+
+    }
+}
