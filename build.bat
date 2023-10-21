29508c99bbcf51648cde0271195a66d33cd1dc7c
Christiaan Rakowski <cs.rakowski@gmail.com>
2023-10-21 21:43:11 +0200

    Attempted to fix healthcheck

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 docker build . --file .\src\FeedManager.Silo\Dockerfile -t feedmanagersilo:cli
-docker build . --file .\src\FeedManager.WebClient\Dockerfile -t feedmanagerweb:cli
+docker build . --file .\src\FeedManager.WebClient\Dockerfile -t feedmanagerwebclient:cli
