96ca0b3c697f113d09717fd57a51b36744bb08c0
Christiaan Rakowski <cs.rakowski@gmail.com>
2024-01-10 20:56:18 +0100

    Updated base image

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-docker build . --file .\src\FeedManager.Silo\Dockerfile -t csrakowski/feedmanagersilo:latest
-docker build . --file .\src\FeedManager.WebClient\Dockerfile -t csrakowski/feedmanagerwebclient:latest
+docker build . --no-cache --file .\src\FeedManager.Silo\Dockerfile -t csrakowski/feedmanagersilo:latest
+docker build . --no-cache --file .\src\FeedManager.WebClient\Dockerfile -t csrakowski/feedmanagerwebclient:latest
